3b0dbcc5dca03dd56e90dc34adeb033af44a5b3e
Daniele Dall'Olio <daniele.dallolio95@gmail.com>
2026-03-11 17:32:40 +0100

    Update dPETImporter.py

--- dPETImporter/dPETImporter.py
+++ dPETImporter/dPETImporter.py
@@ -4,6 +4,8 @@ class dPETImporter:
   def __init__(self, parent):
     parent.title = "DICOM dPET Import Plugin"
     parent.categories = ["Developer Tools.DICOM Plugins"]
+    parent.contributors = ["Daniele Dall'Olio (University of Bologna)"]
+    parent.helpText = "DICOM plugin to import dynamic PET datasets as volume sequences."
     parent.hidden = True
 
     from dPETImporterPlugin import dPETImporterPluginClass
@@ -12,4 +14,5 @@ class dPETImporter:
       slicer.modules.dicomPlugins
     except AttributeError:
       slicer.modules.dicomPlugins = {}
+
     slicer.modules.dicomPlugins['dPETImporterPlugin'] = dPETImporterPluginClass
